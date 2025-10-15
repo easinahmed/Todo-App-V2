399c5662c065a5cef61880532c2c4aad923495d4
easinahmed <easintshirtdesigner@gmail.com>
2025-10-15 23:24:54 +0600

    Inital Project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold">Hello, Tailwind CSS!</h1>
+    </div>
+  )
+}
+
+export default App
